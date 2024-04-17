8a0da59e2a4d33122ef3f03aafe3a59dac7aac9e
BrindhaSelvaraj <itzmibrindha@gmail.com>
2024-04-17 12:23:08 +0530

    1

--- routes.js
+++ routes.js
@@ -6,11 +6,10 @@ const model = require('./model/userSchema')
 
 const router = express.Router();
 
-
-//a middleware funtion to verify user's authorization with tokens before logging them into the tasks page
+//a middleware function to verify user's authorization with tokens before logging them into the tasks page
 const verifyUser = (req, res, next) => {
     const token = req.cookies.token
-    if ( !token ) {
+    if (!token) {
         return res.send('not-token')
     } else {
         jwt.verify(token, process.env.MY_SECRET, (err, decode) => {
@@ -24,7 +23,6 @@ const verifyUser = (req, res, next) => {
     }
 }
 
-
 //routes to create a new user
 router.post('/signup', async (req, res) => {
     const newUser = new model({
@@ -36,83 +34,76 @@ router.post('/signup', async (req, res) => {
     try {
         await newUser.save()
         res.send(newUser)
-    }
-
-    catch (error) {
+    } catch (error) {
         res.status(400)
-        res.send({error: error.message})
+        res.send({ error: error.message })
     }
 })
 
 //Login route and user's authentication with password and email check and authentication with web token
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
-    const token = jwt.sign( {email}, process.env.MY_SECRET, { expiresIn: '1d'} )
+    const token = jwt.sign({ email }, process.env.MY_SECRET, { expiresIn: '1d' })
     try {
-        const user = await model.findOne({ email: email})
-        if ( user.password === password) {
+        const user = await model.findOne({ email: email })
+        if (user.password === password) {
             res.cookie('token', token)
             res.send(user)
-        } 
-        
-    }
-    catch (error) {
+        }
+
+    } catch (error) {
         res.status(401)
-        res.send({error: error.message})
+        res.send({ error: error.message })
     }
 })
 
 //route to get user and user's projects after authorization is confirmed 
 router.get('/tasks', verifyUser, async (req, res) => {
-
-    try{
-        const userDetails = await model.findOne({ email: req.body.email})
+    try {
+        const userDetails = await model.findOne({ email: req.body.email })
         res.send(userDetails)
-    }
-
-    catch (error) {
+    } catch (error) {
         res.status(400)
-        res.send({ error: error.message})
+        res.send({ error: error.message })
     }
-
 })
 
-
 //route to create and add new project/tasks to the user's list
-router.patch('/tasks', verifyUser,  async (req, res) => {
-    
+router.patch('/tasks', verifyUser, async (req, res) => {
+
     try {
         const email = req.body.email
         const update = req.body
-        const option = { new: true}
+        const option = { new: true }
 
         const result = await model.findOneAndUpdate(
-            {email}, { '$push' : { tasks: update}}, option
+            { email }, { '$push': { tasks: update } }, option
         )
         res.send(result)
     }
-    
+
     catch (error) {
         res.status(400)
-        res.send({error: error.message})
+        res.send({ error: error.message })
     }
 })
-
 
 //route to edit user's tasks based on title, description and deadline
 router.patch('/tasks/edit', verifyUser, async (req, res) => {
     try {
         const email = req.body.email
         const update = req.body
-        const option = { new: true}
+        const option = { new: true }
 
         const result = await model.findOneAndUpdate(
-            { email, 'tasks._id': update._id}, { '$set' : { 
+            { email, 'tasks._id': update._id }, {
+            '$set': {
                 'tasks.$.title': update.title,
                 'tasks.$.description': update.description,
                 'tasks.$.deadline': update.deadline,
                 'tasks.$.isDone': update.isDone
-            }}, option
+            }
+        }, option
         )
         res.send(result)
     }
@@ -121,54 +112,50 @@ router.patch('/tasks/edit', verifyUser, async (req, res) => {
     }
 })
 
-
 //route to update user's status to true of false
-router.patch('/tasks/status', verifyUser,  async (req, res) => {
-    
+router.patch('/tasks/status', verifyUser, async (req, res) => {
+
     try {
         const email = req.body.email
         const update = req.body
-        const option = { new: true}
+        const option = { new: true }
 
         const result = await model.findOneAndUpdate(
-            {email, 'tasks._id': update._id}, { '$set' : { 'tasks.$.isDone': update.isDone }}, option
+            { email, 'tasks._id': update._id }, { '$set': { 'tasks.$.isDone': update.isDone } }, option
         )
         res.send(result)
     }
-    
+
     catch (error) {
         res.status(400)
-        res.send({error: error.message})
+        res.send({ error: error.message })
     }
 })
-
 
 //route to delete selected project/tasks from the user's list
 router.patch('/tasks/delete', verifyUser, async (req, res) => {
-    
+
     try {
         const email = req.body.email
-        const {title, description, deadline, isDone, _id } = req.body
-        const option = { new: true}
+        const { title, description, deadline, isDone, _id } = req.body
+        const option = { new: true }
 
         const result = await model.findOneAndUpdate(
-            {email}, { '$pull' : { tasks: {title, description, deadline, isDone, _id }}}, option
+            { email }, { '$pull': { tasks: { title, description, deadline, isDone, _id } } }, option
         )
         res.send(result)
     }
-    
+
     catch (error) {
         res.status(400)
-        res.send({error: error.message})
+        res.send({ error: error.message })
     }
 })
-
 
 //logging out a user
 router.get('/logout', (req, res) => {
     res.clearCookie('token');
-    res.send('success')    
+    res.send('success')
 })
-
 
 module.exports = router
